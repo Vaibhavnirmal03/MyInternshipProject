eae4a51e9052ee080adde27fbfca79d0b4bdf5a1
Vaibhav Nirmal <142920812+Vaibhavnirmal03@users.noreply.github.com>
2024-01-04 22:35:20 +0530

    LinkShortener.java

--- LinkShortener.java
+++ LinkShortener.java
@@ -51,8 +51,7 @@ public class LinkShortener {
         LinkShortener linkShortener = new LinkShortener();
 
         
-//        String longUrl = "https://www.example.com";
-        String longUrl ="http://chart.apis.google.com/chart?chs=500x500&chma=0,0,100,100&cht=p&chco=FF0000%2CFFFF00%7CFF8000%2C00FF00%7C00FF00%2C0000FF&chd=t%3A122%2C42%2C17%2C10%2C8%2C7%2C7%2C7%2C7%2C6%2C6%2C6%2C6%2C5%2C5&chl=122%7C42%7C17%7C10%7C8%7C7%7C7%7C7%7C7%7C6%7C6%7C6%7C6%7C5%7C5&chdl=android%7Cjava%7Cstack-trace%7Cbroadcastreceiver%7Candroid-ndk%7Cuser-agent%7Candroid-webview%7Cwebview%7Cbackground%7Cmultithreading%7Candroid-source%7Csms%7Cadb%7Csollections%7Cactivity|Chart";
+        String longUrl = "https://www.exampleabcdegdsvjnxbcsjgdchvsjcjkshhskdjc27635482gds876rwrdchj63rdbfr737ghdfe645gh.com";
 
         String shortUrl = linkShortener.shortenUrl(longUrl);
         System.out.println("Shortened URL: " + shortUrl);
